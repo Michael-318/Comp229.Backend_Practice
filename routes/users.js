e4a846fe8109f75cdcbd9b067cdc6f18b275d901
Michael-318 <maikoasfeha@gmail.com>
2024-10-22 23:44:13 -0400

    Part 03 - Complete CRUD operations

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-let usersController = require('../controllers/users')
+let usersController = require('../controllers/users');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/create', usersController.create)
+router.get('/list', usersController.list);
+router.post('/create', usersController.create);
+router.get('/get/:userID', usersController.userGet,usersController.userById);
+router.put('/edit/:userID', usersController.update);
+router.delete('/delete/:userID', usersController.remove);
 
 module.exports = router;
